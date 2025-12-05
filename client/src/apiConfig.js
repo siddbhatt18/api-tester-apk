675e133066f84b236730bcdfe960af8ff29ffcbd
Siddharth Bhattacharya <111268102+siddbhatt18@users.noreply.github.com>
2025-12-05 16:54:16 +0530

    Update apiConfig.js

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -2,6 +2,6 @@
 const isProduction = import.meta.env.MODE === 'production';
 
 // REPLACE 'YOUR_RENDER_URL' WITH THE URL YOU JUST COPIED FROM RENDER
-const PROD_URL = 'https://api-tester-backend.onrender.com'; 
+const PROD_URL = 'https://api-tester-backend-4na2.onrender.com/'; 
 
 export const API_BASE_URL = isProduction ? PROD_URL : 'http://localhost:5000';
